1be291268217a7c6b4cd7b74a5e82bcac146f8c8
Aaron Parry <aaronparry@Aarons-MBP.attlocal.net>
2020-04-22 16:00:35 -0400

    cleanup

--- src/components/exercises/utils/search_bar/search_bar_container.js
+++ src/components/exercises/utils/search_bar/search_bar_container.js
@@ -1,6 +1,6 @@
 import {connect} from 'react-redux';
 
-import {setFilter} from '../../../../actions';
+import {setFilter} from '../../../../actions/exercises';
 
 import {SearchBar} from './search_bar'
 
